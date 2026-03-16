312734e4abf0293e78ff7549c29148dc41716580
Jeff Triplett <jeff.triplett@gmail.com>
2026-03-15 20:03:55 -0500

    use whitenoise WSGI wrapper to serve both static and media files

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include
 from django.urls import path
@@ -32,4 +31,3 @@ if settings.DEBUG:
     urlpatterns += [
         path("__reload__/", include("django_browser_reload.urls")),
     ]
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
